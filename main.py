802cb80d04a8bcc59fdb28f8f58dc7ee2a7ae068
sibby <sarah.ibsen@hotmail.com>
2025-02-09 12:30:04 -0600

    Added the correct statistics to the different data types, as well as the associated visualization graphs

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ class nominalStatistics:
     def mode(self):
         stat = statistic(self.data)
         return stat.mode()
+    
+    def frequency(self):
+        stat = statistic(self.data)
+        return stat.frequency()
+    '''
+    Plots associated with this data type
+    '''
+    def piechart(self):
+        stat = statistic(self.data)
+        return stat.piechart()
+    def bargraph(self):
+        stat = statistic(self.data)
+        return stat.bargraph()
+    
 
 class ordinalStatistics:
     '''
@@ -22,9 +36,26 @@ class ordinalStatistics:
     def __init__(self, data):
         self.data = data
     
-    def median(self):
+    def proportion(self):
         stat = statistic(self.data)
-        return stat.median()
+        return stat.proportion()
+    def frequency(self):
+        stat = statistic(self.data)
+        return stat.frequency()
+    def percentiles(self):
+        stat = statistic(self.data)
+        return stat.percentiles()
+    
+    '''
+    Plots associated with this data type
+    '''
+    def piechart(self):
+        stat = statistic(self.data)
+        return stat.piechart()
+    def bargraph(self):
+        stat = statistic(self.data)
+        return stat.bargraph()
+
 
 class discreteStatistics:
     def __init__(self, data):
@@ -34,13 +65,29 @@ class discreteStatistics:
         stat = statistic(self.data)
         return stat.mean()
     
-    def variance(self):
+    def median(self):
         stat = statistic(self.data)
-        return stat.variance()
-
-    def standard_deviation(self):
+        return stat.median()
+    def mode(self):
         stat = statistic(self.data)
-        return stat.standardDeviation()
+        return stat.mode()
+    def frequency(self):
+        stat = statistic(self.data)
+        return stat.frequency()
+    def range(self):
+        stat = statistic(self.data)
+        return stat.range()
+    
+    '''
+    plots associated with this data type
+    '''
+    def piechart(self):
+        stat = statistic(self.data)
+        return stat.piechart()
+    def bargraph(self):
+        stat = statistic(self.data)
+        return stat.bargraph()
+    
 
 class continuousStatistics:
     def __init__(self, data):
@@ -50,14 +97,41 @@ class continuousStatistics:
         stat = statistic(self.data)
         return stat.mean()
     
-    def variance(self):
+    def median(self):
         stat = statistic(self.data)
-        return stat.variance()
+        return stat.median()
+    
+    def mode(self):
+        stat = statistic(self.data)
+        return stat.mode()
     
     def standard_deviation(self):
         stat = statistic(self.data)
         return stat.standardDeviation()
 
-    def coefficient_of_variation(self):
+    def percentiles(self):
         stat = statistic(self.data)
-        return stat.coefficientOfVariation()
+        return stat.percentiles()
+    
+    def range(self):
+        stat = statistic(self.data)
+        return stat.range()
+    
+    # check these two if they are calculated correctly
+    def correlation(self, data2):
+        stat = statistic(self.data)
+        return stat.correlation(data2)
+    def spearman_correlation(self, data2):
+        stat = statistic(self.data)
+        return stat.spearman_correlation(data2)
+    
+    '''
+    Plots associated with this data type
+    '''
+    def histogram(self):
+        stat = statistic(self.data)
+        return stat.histogram()
+    def boxplot(self):
+        stat = statistic(self.data)
+        return stat.boxplot()
+    
